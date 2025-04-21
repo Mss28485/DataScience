f7f3069bbd0037dbef492b3783351529fcb6129a
Mss28485 <mvsreddy@gmail.com>
2025-04-21 19:15:22 +0530

    base

--- LinearAlgebraPartOne/LinearAlgebraPowerCal.py
+++ LinearAlgebraPartOne/LinearAlgebraPowerCal.py
@@ -7,29 +7,25 @@ class LinearAlgPowerCalculation:
     def __init__(self):
         pass
 def main():
-    get_models_power()
+    get_models_power_cal()
 def avg_power():
     avg_power_generation_one_two = (LinearAlgPowerCalculation.model_one+LinearAlgPowerCalculation.model_two)/2
     return avg_power_generation_one_two
-
-def get_models_power():
-    model_one__two_start = (LinearAlgPowerCalculation.model_one * LinearAlgPowerCalculation.model_two_start)
-    model_one__total_at_model_one__two_start = (model_one__two_start/LinearAlgPowerCalculation.model_two)+model_one__two_start
-    model_one = model_one__total_at_model_one__two_start
-    model_two = model_one__two_start
-    print(f'Model One at Model Two Start: {model_one__two_start}')
-    print(f'Model One at Model Two Reach, Model two start point : {model_one__total_at_model_one__two_start}')
-    while model_two < model_one :
-        model_one += LinearAlgPowerCalculation.model_one
-        model_two += LinearAlgPowerCalculation.model_two
-        print(model_two+avg_power())
-        if model_one<model_two:
-            model_one -=.5
-            model_two -=2
-            if model_one==model_two:
-                print(f'Number of days to reach same production of power :{model_two} kj')
-                print(f'Total production @ {model_two} : {model_one+model_two} kj')
-            break
-
+def model_one_total():
+    model_one_total_two = (LinearAlgPowerCalculation.model_one * LinearAlgPowerCalculation.model_two_start)
+    return model_one_total_two
+def model_two_total():
+     model_two_total_one = (model_one_total()/LinearAlgPowerCalculation.model_two)+model_one_total()
+     return model_two_total_one
+def get_models_power_cal():
+    #print(f'Model One at Model Two Start: {model_two_total()}')
+    #print(f'Model One at Model Two Reach, Model two start point @ {model_two_total()} : {model_one_total()}')
+    while model_one_total() < model_two_total() :
+        model_one = model_two_total()+(avg_power() *1)
+        model_two = model_one_total()+(avg_power() * 4)
+        if model_one==model_two:
+            print(f'Number of days to reach same production of power :{model_two} kj')
+            print(f'Total production of two : {model_one+model_two} kj')
+        break
 if __name__ == '__main__':
     main()
