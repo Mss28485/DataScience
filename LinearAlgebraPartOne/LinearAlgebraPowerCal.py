a818bd91680c5c623ee13c5b6ce0646999db0cd6
Mss28485 <mvsreddy@gmail.com>
2025-04-22 10:16:46 +0530

    base

--- LinearAlgebraPartOne/LinearAlgebraPowerCal.py
+++ LinearAlgebraPartOne/LinearAlgebraPowerCal.py
@@ -1,5 +1,4 @@
 #  Copyright (c) 2025.
-import numpy as np
 class LinearAlgPowerCalculation:
     model_one = 1
     model_two = 4
@@ -8,6 +7,9 @@ class LinearAlgPowerCalculation:
         pass
 def main():
     get_models_power_cal()
+def avg_loop():
+    for x in range(10):
+        pass
 def avg_power():
     avg_power_generation_one_two = (LinearAlgPowerCalculation.model_one+LinearAlgPowerCalculation.model_two)/2
     return avg_power_generation_one_two
@@ -18,14 +20,15 @@ def model_two_total():
      model_two_total_one = (model_one_total()/LinearAlgPowerCalculation.model_two)+model_one_total()
      return model_two_total_one
 def get_models_power_cal():
-    #print(f'Model One at Model Two Start: {model_two_total()}')
-    #print(f'Model One at Model Two Reach, Model two start point @ {model_two_total()} : {model_one_total()}')
-    while model_one_total() < model_two_total() :
-        model_one = model_two_total()+(avg_power() *1)
-        model_two = model_one_total()+(avg_power() * 4)
+     while model_one_total() < model_two_total() :
+        model_one = model_two_total()+(avg_power() * LinearAlgPowerCalculation.model_one)
+        model_two = model_one_total()+(avg_power() * LinearAlgPowerCalculation.model_two)
         if model_one==model_two:
             print(f'Power production of Model One :{model_one} and Power production of Model Two :{model_two}  kj')
             print(f'Total production of Model One and Model Two : {model_one+model_two} kj')
+            print(f'Model One Total Days : {model_one} , Model Two Total Days : {model_two-LinearAlgPowerCalculation.model_two_start} ')
+        else:
+            print(f'Model One : {model_one} and Model Two : {model_two} never meet ')
         break
 if __name__ == '__main__':
     main()
