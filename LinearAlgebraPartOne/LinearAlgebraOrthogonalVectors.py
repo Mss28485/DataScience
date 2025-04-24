1509c9e3c25e93674d31fbfc1f044c59270b38cf
Mss28485 <mvsreddy@gmail.com>
2025-04-24 15:27:47 +0530

    base

---
+++ LinearAlgebraPartOne/LinearAlgebraOrthogonalVectors.py
@@ -0,0 +1,15 @@
+#  Copyright (c) 2025.
+import numpy as np
+from numpy import ma
+class LinearAlgOrthogonalVector:
+    def __init__(self,orthogonal_vector_x,orthogonal_vector_y):
+        self.orthogonal_vector_x = np.array(orthogonal_vector_x)
+        self.orthogonal_vector_y = np.array(orthogonal_vector_y)
+    def orthogonal_vector(self):
+        print(f'Values of x and y :: {self.orthogonal_vector_x,self.orthogonal_vector_y}')
+        print(f'Value of dot product :: {np.dot(self.orthogonal_vector_x,self.orthogonal_vector_y)}')
+def main():
+    orthogonal_vect = LinearAlgOrthogonalVector([1,0],[0,1])
+    orthogonal_vect.orthogonal_vector()
+if __name__ == '__main__':
+    main()
